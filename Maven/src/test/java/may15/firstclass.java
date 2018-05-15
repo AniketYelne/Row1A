a13c6c6323f63aadf9b3e20f53b013e618997d29
TRNG <TRNG@10.80.12.114>
2018-05-15 15:03:51 +0530

    jjk

--- Maven/src/test/java/may15/firstclass.java
+++ Maven/src/test/java/may15/firstclass.java
@@ -4,6 +4,14 @@ public class firstclass
 {
   public void f1()
   {
+
 	  System.out.println("Show me some error");
+
+	  System.out.println("hiiiii....1");
+	  
+	  System.out.println("hiiiii....2");
+	  
+	  System.out.println("Hello Aniket");
+
   }
 }
